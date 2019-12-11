4f14ea6a9588660905e0bdae362141db6675244f
QzarD <a---angel@mail.ru>
2019-12-11 13:33:00 +0700

    Updates

---
+++ precache-manifest.20f3c4a177b98e86cfa2ae766ed399d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4e6fb986be8d00bee20c7b87e7c92c",
+    "url": "/myboard/index.html"
+  },
+  {
+    "revision": "b1d168aacb0df20232c3",
+    "url": "/myboard/static/css/main.e3db1412.chunk.css"
+  },
+  {
+    "revision": "f3b9db2398bf9ad299c3",
+    "url": "/myboard/static/js/2.d9c2130c.chunk.js"
+  },
+  {
+    "revision": "b1d168aacb0df20232c3",
+    "url": "/myboard/static/js/main.58a9c831.chunk.js"
+  },
+  {
+    "revision": "021845d2ab7d7c797f77",
+    "url": "/myboard/static/js/runtime-main.f9edf8d1.js"
+  }
+]);
